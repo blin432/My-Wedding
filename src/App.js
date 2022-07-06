a1fb4e2b6e052fcb67b99b8397bd0b80be9fef30
benjamin lin <blin432@gmail.com>
2022-07-05 21:51:29 -0400

    added rsvp modal and password modal

--- src/App.js
+++ src/App.js
@@ -8,8 +8,17 @@ import WeddingParty from './Components/WeddingParty';
 import Rsvp from './Components/Rsvp';
 import Registry from './Components/Registry';
 import Gallery from './Components/Gallery';
+import Example from './Components/Example';
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 
+import { Form } from "react-bootstrap";
+import SitePasswordModal from './Components/SitePasswordModal';
 function App() {
+  const [show, setShow] = useState(true);
+ 
+  const handleClose = () => setShow(false);
   return (
     <div className="App">
         <Navbar />
@@ -18,8 +27,29 @@ function App() {
         <WeddingParty />
         <Gallery/>
         <Registry />
-        <Rsvp />
         <Footer/>
+        {/* <div className="password-cover" show={show} onHide={handleClose}>
+          123123123
+          <div variant="primary" onClick={handleClose}>
+              Save Changes
+          </div>
+        </div> */}
+        <div className="site-password-modal-container"  data-backdrop="static" show={show} onHide={handleClose}>
+          <div className ="site-password-modal">
+                  <Form> 
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlTextarea1"
+                  >
+                    <Form.Label>Example textarea</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
+                  </Form.Group>
+                </Form>
+                <Button variant="primary" onClick={handleClose}>
+                  Save Changes
+                </Button>  
+          </div>
+        </div>
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,20 +1,67 @@
 import React, { Component } from 'react';
+import Example from './Example';
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
+
+import { Form } from "react-bootstrap";
 
 function Navbar() {
+   const [show, setShow] = useState(false);
+ 
+   const handleClose = () => setShow(false);
+   const handleShow = () => setShow(true);
+ 
    return (
-      <nav id="nav-wrap">
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+      <div>
+         <nav id="nav-wrap">
+            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+            <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#eventDetails">Event Details</a></li>
-	         <li><a className="smoothscroll" href="#weddingParty">Wedding Party</a></li>
-            <li><a className="smoothscroll" href="#gallery">Gallery</a></li>
-            <li><a className="smoothscroll" href="#registry">Registry</a></li>
-            <li><a className="smoothscroll" href="#rsvp">RSVP</a></li>
-         </ul>
-      </nav>
+            <ul id="nav" >
+               <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+               <li><a className="smoothscroll" href="#eventDetails">Event Details</a></li>
+               <li><a className="smoothscroll" href="#weddingParty">Wedding Party</a></li>
+               <li><a className="smoothscroll" href="#gallery">Gallery</a></li>
+               <li><a className="smoothscroll" href="#registry">Registry</a></li>
+               <li><a className="smoothscroll rsvp-button"  onClick={handleShow}>RSVP</a></li>
+            </ul>
+
+         </nav>
+
+         <Modal  show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+               <Modal.Title>Hello Guest</Modal.Title>
+             </Modal.Header>
+            <Modal.Body>
+               <p>Will I see you soon?</p>
+               <div >
+                  <div className="guest-rsvp">
+                     <p>Guest 1</p>
+                     <select name="guest1" id="guest1">
+                        <option value="Yes">YES!</option>
+                        <option value="No">NO</option>
+                     </select>
+                  </div>
+                  <div className="guest-rsvp">  
+                     <p>Guest 2</p>
+                     <select name="guest2" id="guest2">
+                        <option value="Yes">YES!</option>
+                        <option value="No">NO</option>
+                     </select>                    
+                  </div>
+               </div>
+            </Modal.Body>
+            <Modal.Footer>
+               <Button variant="secondary" onClick={handleClose}>
+                  Close
+               </Button>
+               <Button variant="primary" onClick={handleClose}>
+                  Save Changes
+               </Button>
+            </Modal.Footer>
+         </Modal>
+      </div>
    );
 }
 
